c8a613d83d44d4bfea1a4eb077283954c5a95c1a
Zehra Khan <khan77040307@gmail.com>
2022-10-26 13:53:37 +0530

    Initialize project using Create React App

---
+++ src/component/ProductList.js
@@ -0,0 +1,126 @@
+import React from "react";
+import "./ProductList.css";
+
+function ProductList() {
+  const [itemList, setItemList] = React.useState([]);
+  React.useEffect(() => {
+    fetch(
+      "https://acciojob-module-7-sept-2022-default-rtdb.asia-southeast1.firebasedatabase.app/products.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        const response = [];
+        for (const key in data) {
+          response.push({
+            id: key,
+            newPrice: data[key].newPrice,
+            oldPrice: data[key].oldPrice,
+            productName: data[key].productName,
+            productImage: data[key].productImage,
+          });
+        }
+        // console.log(response);
+        setItemList(response);
+      });
+  }, []);
+
+  return (
+    <div className="ProductList-container">
+      <div className="ProductList-header">Products</div>
+      <div className="ProductList-list">
+        {itemList &&
+          itemList.map((item) => (
+            <div className="item-container" key={item.id}>
+              <img src={item.productImage} alt="" />
+              <div className="item-info">
+                <div className="item-title">{item.productName}</div>
+
+                <div className="item-footer">
+                  <div className="icons">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-5 h-5 rating"
+                    >
+                      <path
+                        className="rating"
+                        fillRule="evenodd"
+                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-5 h-5 rating"
+                    >
+                      <path
+                        className="rating"
+                        fillRule="evenodd"
+                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-5 h-5 rating"
+                    >
+                      <path
+                        className="rating"
+                        fillRule="evenodd"
+                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-5 h-5 rating"
+                    >
+                      <path
+                        className="rating"
+                        fillRule="evenodd"
+                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-5 h-5 rating"
+                    >
+                      <path
+                        className="rating"
+                        fillRule="evenodd"
+                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <div className="prices">
+                    <div className="item-old-price">{item.oldPrice}/-</div>
+                    <div className="item-price">{item.newPrice}/-</div>
+                  </div>
+                </div>
+
+                <div className="item-footer-btn ">
+                  <button className="item-card-btn btn-primary">
+                    ADD TO CART
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProductList;
